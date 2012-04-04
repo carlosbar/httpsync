1e769206ee9f6d575d9326b333e1cbbfb4a3efdd
carlosbar <carlosbar@gmail.com>
2012-04-04 13:01:38 +0000

    fixing for initial import

--- src/com/locus/httpsync/Httpd.java
+++ src/com/locus/httpsync/Httpd.java
@@ -27,7 +27,7 @@ import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-import com.locus.httpSync.R;
+import com.locus.httpsync.R;
 
 import android.app.Service;
 import android.content.Intent;
